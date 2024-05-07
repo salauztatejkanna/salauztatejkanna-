c2c6a63021b87c64bff5ac690f1e8109c55cc356
salauztatejkana <serveritis3@gmail.com>
2024-05-07 23:26:03 +0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,124 @@
+
+
+
+var e_t = document.getElementById("teksts");
+
+function onload(){
+  e_t = document.getElementById("teksts");
+}
+
+function teksts(muskulis, tekstins){
+  e_t.innerHTML = "<center><h1>"+ muskulis  +"</h1></center>"
+  e_t.innerHTML += "<p>" + tekstins + "</p>"
+}
+
+
+function Trapezijs(){
+  e_t.innerHTML = "<center><h1>"+ "Trapecveida muskulis"  +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-row-unilateral-side.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-bodyweight-pullup-side.mp4#t=0.1"></video>
+  
+  `
+}
+
+function Triceps(){
+   e_t.innerHTML = "<center><h1>"+ "Triceps" +"</h1></center>"
+   e_t.innerHTML += `
+   <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-dumbbell-skullcrusher-side_bgn7Uzz.mp4#t=0.1"></video><br>
+   <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-bodyweight-tricep-dips-front.mp4#t=0.1"></video>
+
+   `
+}
+
+function Apakšdelmi(){
+   e_t.innerHTML = "<center><h1>"+ "Apakšdelmi" +"</h1></center>"
+    e_t.innerHTML += `
+    <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-reverse-curl-side.mp4#t=0.1"></video><br>
+    <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-row-unilateral-side.mp4#t=0.1"></video>
+    `
+}
+
+function Lielais_gūžas_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Lielais gūžas muskulis" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-squat-front.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-hip-thrust-side.mp4#t=0.1"></video>
+  `
+}
+function Augšstilba_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Augšstilba muskulis" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-machine-hamstring-curl-side.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-low-bar-good-morning-front.mp4#t=0.1"></video>
+  `
+}
+
+function Ikru_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Ikru muskulis" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-calf-raises-front.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-calves-stretch-variation-1-side.mp4#t=0.1"></video>
+  `
+}
+
+function Deltoīdi(){
+  e_t.innerHTML = "<center><h1>"+ "Deltoīdi" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-barbell-overhead-press-side_1DIUbfS.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-dumbbell-incline-bench-press-side_2HBfFN3.mp4#t=0.1"></video>
+  `
+}
+function Biceps(){
+  e_t.innerHTML = "<center><h1>"+ " Biceps" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-bodyweight-chinup-side.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-curl-side.mp4#t=0.1"></video>
+  `
+}
+function Lielais_krūšu_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Lielais krūšu muskulis" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Machine-machine-pec-fly-front.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Bodyweight-push-up-side.mp4#t=0.1"></video>
+  `
+}
+
+function Slīpais_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Slīpais muskulis" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-bodyweight-hand-side-plank-side.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-russian-twist-front.mp4#t=0.1"></video>
+  `
+}
+function Presīte(){
+  e_t.innerHTML = "<center><h1>"+ "Presīte" +"</h1></center>"
+  e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-situp-front.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Dumbbells-dumbbell-russian-twist-front.mp4#t=0.1"></video>
+  `
+}
+function Ciskas(){
+  e_t.innerHTML = "<center><h1>"+ "Platais muguras muskulis"  +"</h1></center>"
+    e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-dumbbell-goblet-squat-side.mp4#t=0.1"></video><br>
+    <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-barbell-step-up-side.mp4#t=0.1"></video>
+    `
+}
+
+function Adduktori(){
+  e_t.innerHTML = "<center><h1>"+ "Platais muguras muskulis"  +"</h1></center>"
+    e_t.innerHTML += `
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Barbell-barbell-squat-front.mp4#t=0.1"></video><br>
+    <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-barbell-step-up-side.mp4#t=0.1"></video>
+    `
+}
+
+function Platais_muguras_muskulis(){
+  e_t.innerHTML = "<center><h1>"+ "Platais muguras muskulis"  +"</h1></center>"
+  e_t.innerHTML += `
+<video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-Cables-cable-pullover-side.mp4#t=0.1"></video><br>
+  <video style = "width:90%;"preload="auto" loop="" tabindex="-1" autoplay="" style="height: 100%; width: 100%; object-fit: cover; object-position: center center; opacity: 1;" src="https://media.musclewiki.com/media/uploads/videos/branded/male-bodyweight-pullup-side.mp4#t=0.1"></video>
+  `
+
+} 
---
+++ statistika_script.js
@@ -0,0 +1,66 @@
+var noklusejuma_lietotajs = {
+  "Svars": 65,
+  "Augums": 100,
+  "Vecums": 66,
+  "vidukļa_apkārtmērs":40,
+  "gurnu_apkārtmērs":30,
+  "Aktivitātes_biežums": "Aktīvs",
+  "Smēķēšanas_status": "Smēķēju",
+  "Dzimums": "Vīrietis",
+  }
+
+try{
+  potencialais_noklusejuma_lietotajs = JSON.parse(window.localStorage.getItem("meta"));
+  a = Object.keys(noklusejuma_lietotajs).sort()
+  b = Object.keys(potencialais_noklusejuma_lietotajs).sort()
+  console.log(a)
+  console.log(b)
+  if (JSON.stringify(a) === JSON.stringify(b)){
+    noklusejuma_lietotajs = potencialais_noklusejuma_lietotajs
+  }
+
+}
+catch (error) {
+  console.log("Neatrada istatijumu !")
+}
+
+
+function statistikas_ielade(){
+  console.log(noklusejuma_lietotajs["Dzimums"])
+  /// BMR 
+  var BMR = 0
+  switch (noklusejuma_lietotajs["Dzimums"]){
+    case "Vīrietis":
+      BMR = 10*noklusejuma_lietotajs["Svars"] + 6.25*noklusejuma_lietotajs["Augums"] - 5*noklusejuma_lietotajs["Vecums"] + 5
+      break
+    case "Sieviete":
+       BMR = 10*noklusejuma_lietotajs["Svars"] + 6.25*noklusejuma_lietotajs["Augums"] - 5*noklusejuma_lietotajs["Vecums"] - 161
+      break
+  }
+  switch (noklusejuma_lietotajs["Aktivitātes_biežums"]){
+    case "Aktīvs":
+       IKP = BMR * 1.725
+      break
+    case "":
+    
+  }
+  
+
+
+  /////////////
+  var statistika_par_mani = {
+    "IIKP": Math.round(IKP) + " kalorijas", 
+    "PVA": BMR,
+    "KMI": ((noklusejuma_lietotajs["Svars"], 2)/noklusejuma_lietotajs["Augums"]).toFixed(2) + " kg²/cm",
+    "VGA":  (noklusejuma_lietotajs["vidukļa_apkārtmērs"]/noklusejuma_lietotajs["gurnu_apkārtmērs"]).toFixed(2),
+  }
+  
+  for (var informacija in statistika_par_mani) {
+    elemnts = document.getElementById(informacija)
+    elemnts.innerHTML = statistika_par_mani[informacija]
+    console.log(informacija)
+  }
+ 
+
+
+}
